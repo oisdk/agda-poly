97e078b2950745f4a7fbcacd325a543be0e8754d
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-08-16 15:05:32 -0400

    removed mono

--- typecheck-perf.sh
+++ typecheck-perf.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 find src -name "*.agdai" -type f -delete
-time find src -name "*.agda" -exec agda {} \;
+echo "Typecheck examples"
+time find src -name "Examples.agda" -exec agda {} \;
